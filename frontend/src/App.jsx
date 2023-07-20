6a8befad5cd194c40371f8a0ff08619ff8cca327
Travis <travislogant@gmail.com>
2023-07-20 10:00:17 +0100

    main path for signup and signin

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+import AuthPage from "./AuthPage";
+import SignupPage from "./SignupPage";
+import ChatsPage from "./ChatsPage";
+
+function App() {
+  const [user, setUser] = useState(undefined);
+  const [signUp, setSignUp] = useState(false);
+
+  const onSignUpClick = () => {
+    setSignUp(true);
+  };
+
+  if (!user) {
+    if (signUp) {
+      return <SignupPage onAuth={(user) => setUser(user)} />;
+    } else {
+      return <AuthPage onAuth={(user) => setUser(user)} onSignUpClick={onSignUpClick} />;
+    }
+  } else {
+    return <ChatsPage user={user} />;
+  }
+}
+
+export default App;
