e2819ac2b676c5498ee119928bcab20572ad5ddd
Travis <travislogant@gmail.com>
2023-07-22 22:24:42 +0100

    react engine for the setup

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,24 @@
-const ChatsPage = () => {
-    return <>chats...</>
-}
+import React from "react";
+
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "78fa73aa-720d-4127-8bff-1f20ba77099a",
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
 
 export default ChatsPage;
